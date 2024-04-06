aa3b8dc4818fbc962c274dfa1025304effa8dd88
Rajat Jindal <rajatjindal83@gmail.com>
2024-04-06 13:23:46 +0530

    remove css
    Signed-off-by: Rajat Jindal <rajatjindal83@gmail.com>

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,6 @@
-import { createResolver } from '@nuxt/kit'
-const { resolve } = createResolver(import.meta.url)
-
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  css: [
-    resolve('./assets/css/main.css')
-  ],
   components: [
     {
       path: './components',
